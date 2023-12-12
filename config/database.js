f07c69915b7da79ba7c874fb0606ce494f66e032
lehoan1810 <hoanhao18102000@gmail.com>
2023-12-12 09:21:48 +0700

    config potsgres strapi

--- config/database.js
+++ config/database.js
@@ -19,9 +19,9 @@ module.exports = ({ env }) => {
         connectionString: env("DATABASE_URL"),
         host: env("DATABASE_HOST", "localhost"),
         port: env.int("DATABASE_PORT", 5432),
-        database: env("DATABASE_NAME", "recruitment"),
-        user: env("DATABASE_USERNAME", "postgres"),
-        password: env("DATABASE_PASSWORD", "1qa2ws#ED"),
+        database: env("DATABASE_NAME", "strapi"),
+        user: env("DATABASE_USERNAME", "strapi_demo"),
+        password: env("DATABASE_PASSWORD", "123456"),
         ssl: env.bool("DATABASE_SSL", false) && {
           key: env("DATABASE_SSL_KEY", undefined),
           cert: env("DATABASE_SSL_CERT", undefined),
